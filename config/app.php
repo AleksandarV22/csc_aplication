5c4946acc12079e77e503a1eb6eae18ebe193339
smartsnake90 <smartsnake90@outlook.com>
2023-12-09 00:00:00 +0300

    define SITE_URL

--- config/app.php
+++ config/app.php
@@ -6,22 +6,25 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'csc');
 
-define('SITE_URL', 'http://localhost/csc-aplication/');
+define('SITE_URL', 'http://localhost/csc_aplication/');
 
 include_once 'DatabaseConnection.php';
 $db = new DatabaseConnection();
 
-function base_url($slug){
-    echo SITE_URL.$slug;
+function base_url($slug)
+{
+    echo SITE_URL . $slug;
 }
 
-function valideteInput($dbcon,$input){
-    return mysqli_real_escape_string($dbcon,$input);
+function valideteInput($dbcon, $input)
+{
+    return mysqli_real_escape_string($dbcon, $input);
 }
 
-function redirect($message,$page){
-    $redirectTO = SITE_URL.$page;
+function redirect($message, $page)
+{
+    $redirectTO = SITE_URL . $page;
     $_SESSION['message'] = "$message";
     header("Location: $redirectTO");
-    exit(0);
+    exit (0);
 }
